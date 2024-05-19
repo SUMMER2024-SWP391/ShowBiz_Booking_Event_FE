b8a7e8ef03e68bedca9b3dd1d7c506ea75e354d9
hoangday185 <jarvietnguyen318@gmail.com>
2024-05-19 11:33:29 +0700

    finish page register ui

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,37 +1,39 @@
 const Footer = () => {
   return (
-    <footer className='py-16'>
+    <footer className='py-16 text-nav_color container'>
       <hr />
-      <div className='mx-auto px-4 flex justify-evenly capitalize'>
+      <div className='mx-auto px-4 flex justify-around capitalize'>
         <div>
-          <div className='mt-4 mb-7 font-bold hover:text-gray-700'>Product</div>
-          <div className='mb-4 hover:text-gray-700'>key features</div>
-          <div className='mb-4 hover:text-gray-700'>pricing</div>
-          <div className='mb-4 hover:text-gray-700'>event ticketing</div>
-          <div className='mb-4 hover:text-gray-700'>booking</div>
-          <div className='mb-4 hover:text-gray-700'>online promotion</div>
-          <div className='mb-4 hover:text-gray-700'>developers</div>
+          <div className='mt-4 mb-7 font-bold hover:text-lightgray'>
+            Product
+          </div>
+          <div className='mb-4 hover:text-lightgray'>key features</div>
+          <div className='mb-4 hover:text-lightgray'>pricing</div>
+          <div className='mb-4 hover:text-lightgray'>event ticketing</div>
+          <div className='mb-4 hover:text-lightgray'>booking</div>
+          <div className='mb-4 hover:text-lightgray'>online promotion</div>
+          <div className='mb-4 hover:text-lightgray'>developers</div>
         </div>
         <div>
-          <div className='mt-4 mb-7 font-bold hover:text-gray-700'>
+          <div className='mt-4 mb-7 font-bold hover:text-lightgray'>
             explore more
           </div>
-          <div className='mb-4 hover:text-gray-700'>how it work</div>
-          <div className='mb-4 hover:text-gray-700'>dowload app</div>
-          <div className='mb-4 hover:text-gray-700'>event promoter</div>
-          <div className='mb-4 hover:text-gray-700'>sell tickets</div>
-          <div className='mb-4 hover:text-gray-700'>event organiser</div>
-          <div className='mb-4 hover:text-gray-700'>
+          <div className='mb-4 hover:text-lightgray'>how it work</div>
+          <div className='mb-4 hover:text-lightgray'>dowload app</div>
+          <div className='mb-4 hover:text-lightgray'>event promoter</div>
+          <div className='mb-4 hover:text-lightgray'>sell tickets</div>
+          <div className='mb-4 hover:text-lightgray'>event organiser</div>
+          <div className='mb-4 hover:text-lightgray'>
             nonprofits & fundraisers
           </div>
         </div>
         <div>
-          <div className='mt-4 mb-7 font-bold hover:text-gray-700'>
+          <div className='mt-4 mb-7 font-bold hover:text-lightgray'>
             contact with us
           </div>
-          <div className='mb-4 hover:text-gray-700'>customer support</div>
-          <div className='mb-4 hover:text-gray-700'>dowload app</div>
-          <div className='mb-4 hover:text-gray-700'>new feeds</div>
+          <div className='mb-4 hover:text-lightgray'>customer support</div>
+          <div className='mb-4 hover:text-lightgray'>dowload app</div>
+          <div className='mb-4 hover:text-lightgray'>new feeds</div>
           <div className='mb-4 flex justify-start'>
             <div className='mr-2'>
               <svg
@@ -45,7 +47,7 @@ const Footer = () => {
                 <path d='M4 2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H4zm0-2h12a4 4 0 0 1 4 4v12a4 4 0 0 1-4 4H4a4 4 0 0 1-4-4V4a4 4 0 0 1 4-4z'></path>
               </svg>
             </div>
-            <span className='hover:text-gray-700'>facebook</span>
+            <span className='hover:text-lightgray'>facebook</span>
           </div>
           <div className='mb-4 flex justify-start items-center hover:text-gray-600'>
             <div className='mr-2 '>
@@ -59,18 +61,18 @@ const Footer = () => {
                 viewBox='0 0 24 24'
               >
                 <path
-                  fill-rule='evenodd'
+                  fillRule='evenodd'
                   d='M22 5.892a8.178 8.178 0 0 1-2.355.635 4.074 4.074 0 0 0 1.8-2.235 8.343 8.343 0 0 1-2.605.981A4.13 4.13 0 0 0 15.85 4a4.068 4.068 0 0 0-4.1 4.038c0 .31.035.618.105.919A11.705 11.705 0 0 1 3.4 4.734a4.006 4.006 0 0 0 1.268 5.392 4.165 4.165 0 0 1-1.859-.5v.05A4.057 4.057 0 0 0 6.1 13.635a4.192 4.192 0 0 1-1.856.07 4.108 4.108 0 0 0 3.831 2.807A8.36 8.36 0 0 1 2 18.184 11.732 11.732 0 0 0 8.291 20 11.502 11.502 0 0 0 19.964 8.5c0-.177 0-.349-.012-.523A8.143 8.143 0 0 0 22 5.892Z'
-                  clip-rule='evenodd'
+                  clipRule='evenodd'
                 />
               </svg>
             </div>
-            <span className='hover:text-gray-700'>twitter</span>
+            <span className='hover:text-lightgray'>twitter</span>
           </div>
           <div className='mb-4 flex justify-start'>
             <div className='mr-2'>
               <svg
-                className='w-5 h-5 text-gray-800 hover:text-gray-700'
+                className='w-5 h-5 text-gray-800 hover:text-lightgray'
                 aria-hidden='true'
                 xmlns='http://www.w3.org/2000/svg'
                 width='24'
@@ -80,13 +82,13 @@ const Footer = () => {
               >
                 <path
                   fill='currentColor'
-                  fill-rule='evenodd'
+                  fillRule='evenodd'
                   d='M3 8a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v8a5 5 0 0 1-5 5H8a5 5 0 0 1-5-5V8Zm5-3a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h8a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H8Zm7.597 2.214a1 1 0 0 1 1-1h.01a1 1 0 1 1 0 2h-.01a1 1 0 0 1-1-1ZM12 9a3 3 0 1 0 0 6 3 3 0 0 0 0-6Zm-5 3a5 5 0 1 1 10 0 5 5 0 0 1-10 0Z'
-                  clip-rule='evenodd'
+                  clipRule='evenodd'
                 />
               </svg>
             </div>
-            <span className='hover:text-gray-700'>instagram</span>
+            <span className='hover:text-lightgray'>instagram</span>
           </div>
         </div>
       </div>
--- src/useRouteElements.tsx
+++ src/useRouteElements.tsx
@@ -1,11 +1,17 @@
+import path from './constants/path'
 import EventList from './pages/EventList/EventList'
 import { useRoutes } from 'react-router-dom'
+import Register from './pages/Register/Register'
 
 const useRouteElements = () => {
   const routeElements = useRoutes([
     {
       path: '/',
       element: <EventList />
+    },
+    {
+      path: path.register,
+      element: <Register />
     }
   ])
   return routeElements
