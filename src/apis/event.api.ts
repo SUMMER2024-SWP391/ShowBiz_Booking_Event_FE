780c0857ba2f8cc25435280bf4c78eb992bc8b58
hoangday185 <jarvietnguyen318@gmail.com>
2024-06-01 11:52:49 +0700

    12321

--- src/apis/event.api.ts
+++ src/apis/event.api.ts
@@ -6,7 +6,15 @@ const eventApi = {
   getListEvent: () =>
     http.get<SuccessResponse<{ events: EventList[] }>>('/events'),
   getEventById: (id: string) =>
-    http.get<SuccessResponse<{ event: Event }>>(`/events/${id}`)
+    http.get<SuccessResponse<{ event: Event }>>(`/events/${id}`),
+  getPendingEventList: () =>
+    http.get<SuccessResponse<{ events: EventList[] }>>(
+      '/admins/get-all/pending-list'
+    ),
+  getAllEventList: () =>
+    http.get<SuccessResponse<{ events: EventList[] }>>(
+      '/admins/get-all/event-list'
+    )
 }
 
 export default eventApi
