c207dfcc8ad3aad869f96fe0bf138707b320c268
hoangday185 <jarvietnguyen318@gmail.com>
2024-06-11 14:57:38 +0700

    fix : env

--- src/utils/getGoogleAuthUrl.ts
+++ src/utils/getGoogleAuthUrl.ts
@@ -1,5 +1,6 @@
 const getGoogleAuthUrl = (): string => {
   const { VITE_GOOGLE_CLIENT_ID, VITE_GOOGLE_REDIRECT_URI } = import.meta.env
+    .production
   const url = 'https://accounts.google.com/o/oauth2/v2/auth'
   const query = {
     client_id: VITE_GOOGLE_CLIENT_ID,
