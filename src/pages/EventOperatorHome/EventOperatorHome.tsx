87b79e5cba151992cb268c06422fbf16d6d0f892
Nguyễn Lê Phương Nam <henrynguyen1553@gmail.com>
2024-06-12 17:15:05 +0700

    merge now

--- src/pages/EventOperatorHome/EventOperatorHome.tsx
+++ src/pages/EventOperatorHome/EventOperatorHome.tsx
@@ -19,36 +19,39 @@ export const EventOperatorHome = () => {
               <div className='w-[96%] md:w-full'>
                 <div className='flex flex-col'>
                   <div className='container-xs relative mt-[-3px] flex flex-col items-start pl-[174px] pr-[633px] md:p-5 md:px-5'>
-                    <Heading size='4xl' as='h1'>
+                    <Heading size='4xl' as='h1' className=''>
                       Event Operator
                     </Heading>
                   </div>
-                  <div role='tablist' className='tabs tabs-bordered'>
+                  <div role='tablist' className='tabs tabs-bordered '>
                     <NavLink
                       role='tab'
-                      className='tab tab-active'
+                      className='tab tab-active !text-white-A700'
                       to='/event-operator/'
                     >
                       Events
                     </NavLink>
-                    <NavLink role='tab' className='tab ' to={''}>
+                    <NavLink role='tab' className='tab !text-white-A700' to={''}>
                       People
                     </NavLink>
-                    <NavLink role='tab' className='tab' to={''}>
+                    <NavLink role='tab' className='tab !text-white-A700' to={''}>
                       Newsletters
                     </NavLink>
-                    <NavLink role='tab' className='tab' to={''}>
+                    <NavLink role='tab' className='tab !text-white-A700' to={''}>
                       Insights
                     </NavLink>
-                    <NavLink role='tab' className='tab' to={''}>
+                    <NavLink role='tab' className='tab !text-white-A700' to={''}>
                       Setting
                     </NavLink>
                   </div>
+                  <div className='mt-10'>
                   <Routes>
                     <Route path='event-list' element={<EventListOperator />} />
                     <Route index element={<Events />} />
                     <Route path='create' element={<CreateEvent />} />
                   </Routes>
+                  </div>
+                  
                 </div>
               </div>
             </div>
