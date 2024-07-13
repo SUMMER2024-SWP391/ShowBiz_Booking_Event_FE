f074645119849d4399791236a013788e8ab2ded7
Nguyễn Lê Phương Nam <henrynguyen1553@gmail.com>
2024-07-13 08:19:51 +0700

    CRUD FormFeedback

---
+++ src/Components/FormFeedback/TableFormFeedback.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+ const TableFormFeedback = () => {
+  return (
+    <div>TableFormFeedback</div>
+  )
+}
+export default TableFormFeedback
--- src/apis/form.api.ts
+++ src/apis/form.api.ts
@@ -1,11 +1,12 @@
 import { EventQuestionType } from 'src/@types/form.type'
+import { SuccessResponse } from 'src/@types/utils.type'
 import http from 'src/utils/http'
 
 export const formAPI = {
   createForm: (
     id: string,
     body: { questions: string[]; type: EventQuestionType }
-  ) => http.post<{}>(`/forms/new/${id as string}`, body),
+  ) => http.post<{}>(`/forms/new/${id}`, body),
 
   updateForm: (
     id: string,
@@ -13,5 +14,16 @@ export const formAPI = {
       questions: Array<{ _id: string; description: string }>
       type: EventQuestionType
     }
-  ) => http.post<{}>(`/forms/update/${id as string}`, body)
+  ) => http.post<{}>(`/forms/update/${id as string}`, body),
+
+  addNewQuestion: (
+    id: string,
+    body: { questions: Array<string>; type: EventQuestionType }
+  ) =>
+    http.post<SuccessResponse<{}>>(
+      `forms/question/update/feed-back/${id}`,
+      body
+    ),
+  deleteQuestion: (id: string) =>
+    http.delete<SuccessResponse<null>>(`forms/question/delete/feed-back/${id}`)
 }
