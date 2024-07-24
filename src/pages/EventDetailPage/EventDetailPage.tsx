2c307900fcfc63590f50ecd47f7fdeab074b0c85
hoangday185 <jarvietnguyen318@gmail.com>
2024-07-24 18:17:09 +0700

    fix: fe display true

--- src/pages/EventDetailPage/EventDetailPage.tsx
+++ src/pages/EventDetailPage/EventDetailPage.tsx
@@ -24,44 +24,12 @@ export default function EventDetailPage() {
   return (
     <div className='flex w-full flex-col items-center '>
       <Header className='' />
-      {data &&
-        !isValidToRegister(
-          data.data.data.event.date_event,
-          data.data.data.event.time_start
-        ) && (
-          <EventDetail
-            event={data.data.data.event}
-            renderProps={<HandleRegisterEvent event={data.data.data.event} />}
-          />
-        )}
-
-      {data &&
-        isValidToRegister(
-          data.data.data.event.date_event,
-          data.data.data.event.time_start
-        ) && (
-          <EventDetail
-            event={data.data.data.event}
-            renderProps={
-              <div className='mt-[37px] flex flex-col items-center gap-[21px] self-stretch rounded-[20px] bg-pink-normail pb-[26px] shadow-md sm:pb-5'>
-                <div className='flex self-stretch rounded-tl-[17px] rounded-tr-[17px] bg-[#E67A5B] px-6 pb-[7px] pt-3 sm:px-5'>
-                  <Heading size='s' as='p' className='!font-semibold'>
-                    Notification
-                  </Heading>
-                </div>
-                <Button
-                  size='lg'
-                  shape='round'
-                  className='min-w-[423px] font-semibold hover:shadow-md sm:px-5 !bg-[#F5222D] text-white-A700'
-                  disabled
-                >
-                  This event was end
-                </Button>
-              </div>
-            }
-          />
-        )}
-
+      {data && (
+        <EventDetail
+          event={data.data.data.event}
+          renderProps={<HandleRegisterEvent event={data.data.data.event} />}
+        />
+      )}
       <Footer className='' />
     </div>
   )
